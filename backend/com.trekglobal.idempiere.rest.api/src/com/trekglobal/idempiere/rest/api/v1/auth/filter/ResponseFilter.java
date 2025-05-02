2b4f488b4921c9a28db0532f9e39e7989e5942b6
shamsiag <shamdesignp3@gmail.com>
2025-05-02 13:02:41 +0300

    add cors headers

--- backend/com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/auth/filter/ResponseFilter.java
+++ backend/com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/auth/filter/ResponseFilter.java
@@ -47,6 +47,14 @@ public class ResponseFilter implements ContainerResponseFilter {
 
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+		// Add CORS headers to allow cross-origin requests
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+		responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+		
+		// Clean up server context
 		ServerContext.dispose();
 	}
 }
